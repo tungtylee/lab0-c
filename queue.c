d81b32d98ba472ccd54d36af86d8a5848eabad5c
rtylee <rtylee@gmail.com>
2020-09-20 01:31:09 +0800

    Implement reversion of queue

--- queue.c
+++ queue.c
@@ -165,8 +165,22 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (q) {
+        list_ele_t *curr, *oldprev, *oldnext;
+        curr = q->head;
+        oldprev = NULL;
+        while (curr) {
+            oldnext = curr->next;
+            curr->next = oldprev;
+            oldprev = curr;
+            curr = oldnext;
+        }
+        // Swapping
+        oldprev = q->head;
+        q->head = q->tail;
+        q->tail = oldprev;
+    }
 }
 
 /*
